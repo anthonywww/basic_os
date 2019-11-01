7c784773d3b627daa8d211272908d6589028d12e
anthonywww <awaldsmith@protonmail.com>
2019-10-31 19:46:11 -0700

    Added bin directory creation

--- assemble.sh
+++ assemble.sh
@@ -4,6 +4,10 @@ INPUT_DIR="src"
 OUTPUT_DIR="bin"
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+if [[ ! -d $DIR/$OUTPUT_DIR ]]; then
+	mkdir -p $DIR/$OUTPUT_DIR
+fi
+
 echo -e "$(tput sgr0)$(tput setaf 2)Assembling boot sector ...$(tput sgr0)"
 cd $DIR/$INPUT_DIR
 nasm -f bin ${DIR}/${INPUT_DIR}/${BOOT_SECTOR} -o ${DIR}/${OUTPUT_DIR}/boot.bin
